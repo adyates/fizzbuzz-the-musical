7dc9162aa934000b8446013cbdf33fd170252ff9
Alvin <mambothief@gmail.com>
2015-04-07 14:52:13 -0500

    add(java): Include missing file changes

---
+++ java/src/main/java/com/mambothief/FizzBuzz.java
@@ -0,0 +1,35 @@
+package com.mambothief;
+
+/**
+ * The main Fizz Buzz class. The creator of pain and torment. For Freedom.
+ * 
+ * @author Alvin Yates
+ */
+
+public class FizzBuzz {
+	/**
+	 * The classic Fizz Buzz function. Given a ceiling, builds the printable
+	 * string in classic FizzBuzz format for the numbers [1, ceiling) and
+	 * returns the resulting buffer.
+	 *
+	 * @param ceiling
+	 *            The number (inclusive) to end on.
+	 */
+	public StringBuilder sodaBee(int ceiling) {
+		StringBuilder sb = new StringBuilder();
+		SodaBuzzFactory factory = new SodaBuzzFactory();
+		for (int nextNum = 1; nextNum <= ceiling; nextNum++) {
+			sb.append(factory.makeSodaBee(nextNum));
+		}
+
+		return sb;
+	}
+
+	public static void main(String[] argv) {
+		// Using this for classic CL testing only
+		FizzBuzz momentOfTruth = new FizzBuzz();
+		StringBuilder answer = momentOfTruth.sodaBee(100);
+		System.out.println(answer);
+	}
+
+}
