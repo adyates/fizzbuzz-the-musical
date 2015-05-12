30abfa5d8bcad62f29238b117ca7960b01039405
Alvin <mambothief@gmail.com>
2015-05-12 17:08:51 -0500

    chore(format): Fix the formatting

--- java/src/main/java/com/mambothief/FizzBuzz.java
+++ java/src/main/java/com/mambothief/FizzBuzz.java
@@ -7,29 +7,28 @@ package com.mambothief;
  */
 
 public class FizzBuzz {
-	/**
-	 * The classic Fizz Buzz function. Given a ceiling, builds the printable
-	 * string in classic FizzBuzz format for the numbers [1, ceiling) and
-	 * returns the resulting buffer.
-	 *
-	 * @param ceiling
-	 *            The number (inclusive) to end on.
-	 */
-	public StringBuilder sodaBee(int ceiling) {
-		StringBuilder sb = new StringBuilder();
-		SodaBuzzFactory factory = new SodaBuzzFactory();
-		for (int nextNum = 1; nextNum <= ceiling; nextNum++) {
-			sb.append(factory.makeSodaBee(nextNum));
-		}
+    /**
+     * The classic Fizz Buzz function. Given a ceiling, builds the printable string in classic
+     * FizzBuzz format for the numbers [1, ceiling) and returns the resulting buffer.
+     *
+     * @param ceiling
+     *            The number (inclusive) to end on.
+     */
+    public StringBuilder sodaBee(int ceiling) {
+        StringBuilder sb = new StringBuilder();
+        SodaBuzzFactory sodaBuzzFactory = new SodaBuzzFactory();
+        for (int nextNum = 1; nextNum <= ceiling; nextNum++) {
+            sb.append(sodaBuzzFactory.makeSodaBee(nextNum));
+        }
 
-		return sb;
-	}
+        return sb;
+    }
 
-	public static void main(String[] argv) {
-		// Using this for classic CL testing only
-		FizzBuzz momentOfTruth = new FizzBuzz();
-		StringBuilder answer = momentOfTruth.sodaBee(100);
-		System.out.println(answer);
-	}
+    public static void main(String[] argv) {
+        // Using this for classic CL testing only
+        FizzBuzz momentOfTruth = new FizzBuzz();
+        StringBuilder answer = momentOfTruth.sodaBee(100);
+        System.out.println(answer);
+    }
 
 }
--- java/src/main/java/com/mambothief/SodaBuzzFactory.java
+++ java/src/main/java/com/mambothief/SodaBuzzFactory.java
@@ -7,24 +7,23 @@ package com.mambothief;
  */
 
 public class SodaBuzzFactory {
-
-	/**
-	 * Create a new String for FizzBuzz usage based on the number passed in.
-	 * Typical FB rules apply (3 for Fizz, 5 for Buzz, 3 and 5 for FizzBuzz).
-	 */
-	public StringBuilder makeSodaBee(int number) {
-		StringBuilder sb = new StringBuilder(); 
-		if (number % 3 != 0 && number % 5 != 0) {
-			sb.append(number);
-		} else {
-			if (number % 3 == 0) {
-				sb.append("Fizz");
-			}
-			if (number % 5 == 0) {
-				sb.append("Buzz");
-			}
-		}
-		sb.append("\n");
-		return sb;
-	}
+    /**
+     * Create a new String for FizzBuzz usage based on the number passed in. Typical FB rules apply
+     * (3 for Fizz, 5 for Buzz, 3 and 5 for FizzBuzz).
+     */
+    public StringBuilder makeSodaBee(int number) {
+        StringBuilder sb = new StringBuilder();
+        if (number % 3 != 0 && number % 5 != 0) {
+            sb.append(number);
+        } else {
+            if (number % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (number % 5 == 0) {
+                sb.append("Buzz");
+            }
+        }
+        sb.append("\n");
+        return sb;
+    }
 }
