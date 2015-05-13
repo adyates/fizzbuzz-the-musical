97c228e0f85e4f8c812e98f143fbb86c9b4e32ac
Alvin <mambothief@gmail.com>
2015-05-13 17:57:58 -0500

    add(nodejs): Add a basic nodejs pipeline

---
+++ nodejs/js/fizzbuzz.js
@@ -0,0 +1,5 @@
+function basicTest(number) {
+    return 4;
+}
+
+console.log("Tada: " + basicTest(4));
---
+++ nodejs/test/fizzbuzz.spec.js
@@ -0,0 +1,5 @@
+describe("Testing framework", function() {
+  it("makes sure that automated tests work", function() {
+    expect(basicTest(4)).toBe(4);
+  });
+});
