556e01dcbd27f832960e7a39e3032a00bb2c39e2
Alvin <mambothief@gmail.com>
2015-05-28 03:51:43 -0500

    add(ci): Add travis.yml and grunt for CI

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt){
+	require('load-grunt-tasks')(grunt);
+	grunt.initConfig({
+		shell: {
+			travisLint: {
+				command: 'travis-lint ./travis.yml'
+			},
+			javaTest: {
+				command: 'gradle test',
+				options: { execOptions: { cwd: 'java'}}
+			},
+			nodejsTest: {
+				command: 'gulp',
+				options: { execOptions: { cwd: 'nodejs'}}
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['shell:javaTest', 'shell:nodejsTest', 'test-web']);
+
+	grunt.registerTask('test-web', 'Tests the HTML5/CSS3 version', function() {
+		grunt.log.writeln('Still need to implement a test process for web/...');
+	});
+};
